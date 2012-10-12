7f155d32f7f5dea0e370658739f40b2373212a8e
Adam Higuera <adam.higuera@colorado.edu>
2012-10-11 18:20:17 -0600

    Added cpp files to test different communication implementations

---
+++ collective_emsquare2d.cpp
@@ -0,0 +1,26 @@
+#include "Simulation.hpp"
+#include <cstdlib>
+#include <sstream>
+#include <unistd.h>
+
+int main(int argc, char* argv []) {  
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+
+  // std::ostringstream startstrm2;
+  // char hostname[520];
+  // gethostname(hostname, 512);
+  // startstrm2 << "Rank " << world.rank() << " is running on host: " <<
+  //   hostname << std::endl;
+  // std::cout << startstrm2.str() << std::endl;
+
+  char *domain_size = std::getenv("DOMAIN_SIZE");
+
+  unsigned int block_size = domain_size ? std::atoi(domain_size) : 50;
+  if (world.rank() == 0)
+    std::cout << "domain_size: " << block_size << std::endl;
+  unsigned int n_cells = block_size*static_cast<unsigned int>(sqrt(world.size()));
+  TEmnInitializer<1,1, CollectiveRHSCollection> init(.1/n_cells, .1/n_cells, .1, .1, block_size, world);
+  Simulation the_simulation(.1, .1, 5e-8, n_cells, 1000, & init, world);
+  the_simulation.simulate(true, 100, 10);
+}
