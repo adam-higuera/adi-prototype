3850966cdb56e5f904c7a4f9ce665e0e5fbcdee9
Adam Higuera <adam.higuera@colorado.edu>
2012-10-15 22:16:04 -0600

    This machine is actually the one with the general version of the script

--- qsemsquare2d-strong.q
+++ qsemsquare2d-strong.q
@@ -2,7 +2,7 @@
 #
 #PBS -l walltime=4:0:0
 #PBS -l nodes=3:ppn=3
-#PBS -q immediate
+#PBS -q lazy
 #PBS -N emsquare2d50_9
 #PBS -o emsq2d25procs.out
 #PBS -e emsq2d25procs.err
@@ -11,4 +11,4 @@
 cd /scr_verus/avh/Development/adi-prototype
 export LD_LIBRARY_PATH=/scr_verus/avh/Development/boost_1_50_0/stage/lib:$LD_LIBRARY_PATH
 #cd $PBS_O_WORKDIR
-/usr/local/mpi/bin/mpiexec emsquare2d-strong-large-atlas --bynodes > str${DOMAIN_SIZE}timing$((${PROCS_PER_EDGE}*${PROCS_PER_EDGE}))atlas
+/usr/local/mpi/bin/mpiexec ${PROG_NAME} --bynodes > ${RESULTS_DIR}/str${DOMAIN_SIZE}timing$((${PROCS_PER_EDGE}*${PROCS_PER_EDGE}))
--- submit-jobs.clj
+++ submit-jobs.clj
@@ -1,9 +1,11 @@
 #!/bin/bash lein-exec
 
 (use '[leiningen.exec :only  (deps)])
-(deps '[[org.clojure/math.numeric-tower "0.0.1"]])
+(deps '[[org.clojure/math.numeric-tower "0.0.1"]
+	[org.clojure/tools.cli "0.2.2"]])
 
 (use 'clojure.math.numeric-tower)
+(use '[clojure.tools.cli :only [cli]])
 (require 'clojure.java.shell)
 
 ; (defn get-resource-list [edge-procs]
@@ -16,14 +18,29 @@
     (str "-l nodes=" (quot (expt edge-procs 2) 7) ":ppn=7+1:ppn=" (rem (expt edge-procs 2) 7))
     (str "-l nodes=" (quot (expt edge-procs 2) 8) ":ppn=8+1:ppn=" (rem (expt edge-procs 2) 8))))
 
-(defn submit-job [domain-size edge-procs]
+(defn submit-job [domain-size edge-procs executable results-directory q]
   (println (get-resource-list edge-procs))
   (clojure.java.shell/sh "qsub"
                          (get-resource-list edge-procs)
-                         (str "-vDOMAIN_SIZE=" domain-size ",PROCS_PER_EDGE=" edge-procs)
-                         (str "-Nemsquare2d" domain-size "_" edge-procs)
+                         (str "-vDOMAIN_SIZE=" domain-size ",PROCS_PER_EDGE=" edge-procs
+                              ",PROG_NAME=" executable
+                              ",RESULTS_DIR=" results-directory)
+                         (str "-N" executable domain-size "_" edge-procs)
+                         (str "-q" q)
                          "/scr_verus/avh/Development/adi-prototype/qsemsquare2d-strong.q"))
 
-(doseq [domain-size [10 20 30 40 50]
-        edge-procs [3 4 5 6 7 8 9 10]]
-  (println (submit-job domain-size edge-procs)))
+(let [args-hash
+      (cli *command-line-args*
+           ["-s" "--domain-sizes" "Run the code with these domain sizes" :parse-fn #(read %)]
+           ["-p" "--edge-procs" "Run the code with these many processors along the edges" :parse-fn #(read %)]
+           ["-e" "--executable-name" "Use this executable"]
+           ["-d" "--results-directory" "Write results to this folder"]
+           ["-q" "--queue" "Submit to this queue"])
+      executable (if (contains? args-hash :executable-name) (:executable-name args-hash) "emsquare2d-strong")
+      results-directory (if (contains? args-hash :results-directory)
+                          (:results-directory args-hash)
+                          ".")
+      q (if (contains? args-hash :queue) (:queue args-hash) "lazy")]
+  (doseq [domain-size (:domain-sizes args-hash)
+          edge-procs (:edge-procs args-hash)]
+    (println (submit-job domain-size edge-procs executable results-directory))))
