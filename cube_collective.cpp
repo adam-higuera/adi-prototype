624b85bcf3d626de78dd96d2198a78aeb478ddc0
Adam Higuera <adam.higuera@colorado.edu>
2013-07-02 07:02:26 -0600

    Forgot to add main().

---
+++ cube_collective.cpp
@@ -0,0 +1,36 @@
+#include "Simulation3D.hpp"
+#include <cstdlib>
+#include <sstream>
+#include <unistd.h>
+#include <cmath>
+
+int main(int argc, char* argv []) {
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+
+  // std::ostringstream startstrm2;
+  // char hostname[520];
+  // gethostname(hostname, 512);
+  // startstrm2 << "Rank " << world.rank() << " is running on host: " <<
+  //   hostname << std::endl;
+  // std::cout << startstrm2.str() << std::endl;
+
+  char *domain_size = std::getenv("DOMAIN_SIZE");
+  char *dump_dir = std::getenv("RESULTS_DIR");
+
+  unsigned int block_size = domain_size ? std::atoi(domain_size) : 50;
+  std::string dump_directory = dump_dir ? std::string(dump_dir) : std::string("");
+
+  if (world.rank() == 0)
+    std::cout << "domain_size: " << block_size << std::endl;
+  unsigned int n_procs = static_cast<unsigned int>(pow(world.size(), 1.0/3.0));
+  unsigned int n_cells = block_size*n_procs;
+  TEmnlInitializer<1,1,1, CollectiveRHSCollection>
+    init(.1/n_cells, .1/n_cells, .1/n_cells, .1, .1, .1,
+	 block_size);
+
+  double simulation_time=10*sqrt(2)*(.1)/LIGHTSPEED; // Run for 10 periods
+  Simulation3D the_simulation(.1, .1, .1, simulation_time, n_cells, 10000,
+			      n_procs, n_procs, n_procs, block_size, dump_directory, & init, world);
+  the_simulation.simulate(false, 100, 10);
+}
